5ea06d3112b15cfc5d6a1ebaed3a0b1fd1713b4f
Lama <lama.aldhafeer@gmail.com>
2023-08-14 22:20:48 +0300

    added graph to budget card

--- lib/globals/data.dart
+++ lib/globals/data.dart
@@ -4,13 +4,42 @@ class Data {
   static bool isExpanded = false;
   static double balance = 0;
   static List<Transaction> transactions = [
-    const Transaction(amount: 100, icon: Icons.abc, title: 'Hello', type: TransactionType.income),
-    const Transaction(amount: 100, icon: Icons.abc, title: 'Hello', type: TransactionType.income),
+    const Transaction(
+        amount: 100,
+        icon: Icons.abc,
+        title: 'Hello',
+        type: TransactionType.income),
+    const Transaction(
+        amount: 100,
+        icon: Icons.abc,
+        title: 'Hello',
+        type: TransactionType.income),
+    const Transaction(
+        amount: 50,
+        icon: Icons.abc,
+        title: 'Hello',
+        type: TransactionType.outcome),
+    const Transaction(
+        amount: 90,
+        icon: Icons.abc,
+        title: 'Hello',
+        type: TransactionType.outcome),
+  ];
+  // starting point from the balance
+  static List<double> outcomeTransactionsRate = [
+    balance / 100,
+  ];
+  static List<double> incomeTransactionsRate = [
+    balance / 100,
   ];
 }
 
 class Transaction {
-  const Transaction({required this.amount, required this.icon, required this.title, required this.type});
+  const Transaction(
+      {required this.amount,
+      required this.icon,
+      required this.title,
+      required this.type});
 
   final IconData icon;
   final String title;

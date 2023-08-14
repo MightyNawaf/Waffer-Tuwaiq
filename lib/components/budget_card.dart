c07dc7c3112c016f5994c4b3bcb46dd853fd11bb
Alawashez2 <mohmmd20111@hotmail.com>
2023-08-15 00:20:59 +0300

    new animation

--- lib/components/budget_card.dart
+++ lib/components/budget_card.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_animate/flutter_animate.dart';
 import 'package:waffer/components/transaction_sheet.dart';
 import 'package:waffer/constants/colors.dart';
 import 'package:waffer/constants/spacings.dart';
@@ -20,105 +19,113 @@ class BudgetCard extends StatefulWidget {
 }
 
 class _BudgetCardState extends State<BudgetCard> {
+  double opacityLevel = 1.0;
   @override
   Widget build(BuildContext context) {
     final height = Data.isExpanded ? context.height / 3 : context.height / 6;
 
-    return InkWell(
-      onTap: () async {
-        Data.isExpanded = !Data.isExpanded;
+    return Stack(
+      children: [
+        InkWell(
+          onTap: () async {
+            Data.isExpanded = !Data.isExpanded;
 
-        setState(() {});
-        context.findAncestorStateOfType<MyHomePageState>()?.setState(() {});
-      },
-      child: AnimatedContainer(
-        duration: const Duration(milliseconds: 100),
-        curve: Curves.easeInOut,
-        height: height,
-        margin: const EdgeInsets.symmetric(horizontal: 24),
-        child: Material(
-          borderRadius: BorderRadius.circular(16),
-          elevation: 5,
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const Text(
-                      'Total',
-                      style: TextStyle(fontWeight: FontWeight.w600),
-                    ),
-                    kVSpace8,
-                    Row(
-                      children: [
-                        Text(
-                          '\$${Data.balance}',
-                          style: const TextStyle(
-                            fontSize: 40,
-                            fontWeight: FontWeight.w600,
+            setState(() {});
+            context.findAncestorStateOfType<MyHomePageState>()?.setState(() {});
+          },
+          child: AnimatedOpacity(
+            opacity: opacityLevel,
+            duration: const Duration(seconds: 2),
+            child: Container(
+              height: height,
+              margin: const EdgeInsets.symmetric(horizontal: 24),
+              child: Material(
+                borderRadius: BorderRadius.circular(16),
+                elevation: 5,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Text(
+                            'Total',
+                            style: TextStyle(fontWeight: FontWeight.w600),
                           ),
-                        ).shimmered,
-                        kHSpace8,
-                        const Icon(
-                          Icons.remove_red_eye,
-                          color: WColors.grey,
-                        )
-                      ],
-                    ),
-                    kVSpace16,
-                    Text(
-                      getLastTransaction,
-                      style: TextStyle(
-                        fontWeight: FontWeight.w600,
-                        color: getLastTransactionColor,
+                          kVSpace8,
+                          Row(
+                            children: [
+                              Text(
+                                '\$${Data.balance}',
+                                style: const TextStyle(
+                                  fontSize: 40,
+                                  fontWeight: FontWeight.w600,
+                                ),
+                              ).shimmered,
+                              kHSpace8,
+                            ],
+                          ),
+                          kVSpace16,
+                          Text(
+                            getLastTransaction,
+                            style: TextStyle(
+                              fontWeight: FontWeight.w600,
+                              color: getLastTransactionColor,
+                            ),
+                          ),
+                        ],
                       ),
-                    ),
-                  ],
-                ),
-                ZoomTapAnimation(
-                  child: InkWell(
-                    onTap: () async {
-                      HapticFeedback.mediumImpact();
-                      await showModalBottomSheet(
-                        context: context,
-                        builder: (context) {
-                          return const AddTransactionSheet();
-                        },
-                      );
-                      //   context.findAncestorStateOfType<MyHomePageState>()?.setState(() {});
-                      widget.onSheetClosed();
-                    },
-                    child: Container(
-                      decoration: const BoxDecoration(
-                        shape: BoxShape.circle,
-                        color: WColors.blue,
+                      ZoomTapAnimation(
+                        child: InkWell(
+                          onTap: () async {
+                            HapticFeedback.mediumImpact();
+                            await showModalBottomSheet(
+                              context: context,
+                              builder: (context) {
+                                return const AddTransactionSheet();
+                              },
+                            );
+                            //   context.findAncestorStateOfType<MyHomePageState>()?.setState(() {});
+                            widget.onSheetClosed();
+                          },
+                          child: Container(
+                            decoration: const BoxDecoration(
+                              shape: BoxShape.circle,
+                              color: WColors.blue,
+                            ),
+                            height: 70,
+                            width: 70,
+                            child: const Icon(
+                              Icons.add,
+                              color: WColors.white,
+                              size: 30,
+                            ),
+                          ),
+                        ),
                       ),
-                      height: 70,
-                      width: 70,
-                      child: const Icon(
-                        Icons.add,
-                        color: WColors.white,
-                        size: 30,
-                      ),
-                    ),
+                    ],
                   ),
                 ),
-              ],
+              ),
             ),
           ),
         ),
-      )
-          .animate()
-          .fade(
-            duration: 500.ms,
-          )
-          .scale(
-            delay: 500.ms,
-          ),
+        Positioned(
+          left: 120,
+          top: 90,
+          child: IconButton(icon: Icon( opacityLevel == 0 ? Icons.visibility_off : Icons.remove_red_eye,
+                color: WColors.grey,
+              ),
+              onPressed: () {
+                setState(() {
+                  opacityLevel = opacityLevel == 0.0 ? 1.0 : 0.0;
+                });
+              }),
+        ),
+      ],
     );
   }
 }
